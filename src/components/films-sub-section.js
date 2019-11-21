655a7d12765de869904a4ae28262e420fe568316
Alexander <al.deimosfrontend@gmail.com>
2019-11-21 23:32:06 +0300

    webpack configuration, add components

---
+++ src/components/films-sub-section.js
@@ -0,0 +1,11 @@
+const createFilmExtraListTemlate = (title) => {
+  return (`
+  <section class="films-list--extra">
+    <h2 class="films-list__title">${title}</h2>
+
+    <div class="films-list__container"></div>
+  </section
+  `);
+};
+
+export {createFilmExtraListTemlate};
